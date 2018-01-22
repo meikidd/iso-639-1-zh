0581230aa85429c681261e64e5f818e27835f051
meikidd <meikidd@gmail.com>
2018-01-22 16:19:28 +0800

    update code style

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import LANGUAGES_ZH_NAMES from './data.js';
 
 export default class ISO6391ZH extends ISO6391 {
   static getLanguages(codes) {
-    return super.getLanguages(codes).map(language =>
+    return super.getLanguages(codes).map(language => {
       Object.assign(language, {
         zhName: ISO6391ZH.getZhName(language.code),
-      }),
-    );
+      });
+      return language;
+    });
   }
 
   static getZhName(code) {
--- test/test.js
+++ test/test.js
@@ -2,7 +2,9 @@ const assert = require('assert');
 const ISO639ZH = require('../build/index');
 const LANGUAGES_ZH_NAMES = require('../build/data');
 
-const zhNameList = Object.keys(LANGUAGES_ZH_NAMES).map(code => LANGUAGES_ZH_NAMES[code]);
+const zhNameList = Object.keys(LANGUAGES_ZH_NAMES).map(
+  code => LANGUAGES_ZH_NAMES[code],
+);
 
 describe('getZhName()', () => {
   it('en', () => assert.equal(ISO639ZH.getZhName('en'), '英语'));
